11f1653770a72b1519867090c36eed333e509ffe
Amin Khaled <aminkhaledbo4@gmail.com>
2025-07-10 21:19:30 +0300

    Added a demo for FTP & SFTP

--- src/test/java/com/example/formatparser/Main.java
+++ src/test/java/com/example/formatparser/Main.java
@@ -1,10 +1,82 @@
 package com.example.formatparser;
 
+import com.jcraft.jsch.*;
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
+    private static void demoSFTP() {
+        try {
+            JSch jsch = new JSch();
+
+            System.out.println("Connecting to SFTP test server...");
+            Session session = jsch.getSession("demo", "test.rebex.net", 22);
+            session.setPassword("password");
+
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            Channel channel = session.openChannel("sftp");
+            channel.connect();
+            ChannelSftp sftpChannel = (ChannelSftp) channel;
+
+            System.out.println("Files on SFTP server:");
+
+            // List files
+            int cnt = 0;
+            sftpChannel.ls("/").forEach(entry -> {
+                ChannelSftp.LsEntry file = (ChannelSftp.LsEntry) entry;
+                System.out.println("- " + file.getFilename());
+            });
+
+            // Download a file
+            sftpChannel.get("readme.txt", "sftp_readme.txt");
+            System.out.println("Downloaded: readme.txt");
+
+            sftpChannel.disconnect();
+            session.disconnect();
+            System.out.println("SFTP operations completed");
+        } catch (Exception e) {
+            System.out.println("SFTP Demo: " + e.getMessage());
+        }
+    }
+
+    private static void demoFTP() {
+        try {
+            FTPClient ftpClient = new FTPClient();
+
+            System.out.println("Connecting to FTP test server...");
+            ftpClient.connect("test.rebex.net", 21);
+            ftpClient.login("demo", "password");
+
+            // List files
+            System.out.println("Files on the server:");
+            String[] files = ftpClient.listNames();
+            int cnt = 0;
+            for (String file : files) {
+                System.out.println(++cnt + "- " + file);
+            }
+
+            // Download a file
+            File downloadedFile = new File("ftp_readme.txt");
+            FileOutputStream fos = new FileOutputStream(downloadedFile);
+            ftpClient.retrieveFile("readme.txt", fos);
+            fos.close();
+            System.out.println("Downloaded: readme.txt");
+
+            ftpClient.logout();
+            ftpClient.disconnect();
+            System.out.println("FTP operations completed");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private static void createSampleFiles() {
         try {
             // Create JSON file
@@ -29,12 +101,13 @@ public class Main {
             System.err.println("Error creating sample files: " + e.getMessage());
         }
     }
+
     public static void main(String[] args) {
         FormatParser parser = new FormatParser();
 
         createSampleFiles();
 
-        // Parse each file type
+        // Part 1: Parse each file type
         parser.parseJSON("sample.json");
         System.out.println();
         parser.parseCSV("sample.csv");
@@ -42,5 +115,13 @@ public class Main {
         parser.parseYAML("sample.yaml");
         System.out.println();
         parser.parseXML("sample.xml");
+        System.out.println();
+
+        // Part 2: FTP DEMO
+        demoFTP();
+        System.out.println();
+
+        // Part 3: SFTP DEMO
+        demoSFTP();
     }
 }
